39a74816dfd1acd49e2f08e212e85c7ace8142cc
Leelsmuth <femianjorin@rocketmail.com>
2020-01-05 14:32:07 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//Load Routes
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
+
+//Passport config
+require('./config/passport')(passport);
+
+//db config
+const db = require('./config/database');
+
+//connect to mongoose
+mongoose
+  .connect(db.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch(err => console.log(err));
+
+//Handlebars Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
+//Express Session middleware
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+//Global Variables
+
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+//Index Route
+app.get('/', (req, res) => {
+  const title = 'Welcome1';
+  res.render('Index', { title: title });
+});
+
+//About route
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+//Use Routes
+app.use('/ideas', ideas);
+app.use('/users', users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
+});
